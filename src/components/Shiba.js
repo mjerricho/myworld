9e9e9c073567d6f57d89f5752531393ed2bdd94e
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2022-02-27 16:08:55 +0800

    shiba

---
+++ src/components/Shiba.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Scene from '../assets/models/shiba/Scene.js'
+
+
+export default function Shiba() {
+  return (
+    <Scene scale={1.5} position={[0, 0.5, 0.5]}/>
+  )
+}
